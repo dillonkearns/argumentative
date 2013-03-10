3406853a83be22c97392524e140d603ccaae5a10
Dillon Kearns <dillon@dillonkearns.com>
2013-03-09 21:39:59 -0800

    Rename method in test cases

--- argumentative_test.rb
+++ argumentative_test.rb
@@ -5,7 +5,7 @@ require_relative 'argumentative'
 describe Argumentative do
   include Argumentative
   it 'raises error when type not handled' do
-    def complicated_method(*args)
+    def flexible_args_method(*args)
       argumentative(args) do
         when_type(Array) do
           raise "Shouldn't evaluate non-matching block"
@@ -17,11 +17,11 @@ describe Argumentative do
       end
     end
 
-    assert_raises(ArgumentError) { complicated_method("some string") }
+    assert_raises(ArgumentError) { flexible_args_method("some string") }
   end
 
   it 'runs clause for type when first matches' do
-    def complicated_method(*args)
+    def flexible_args_method(*args)
       argumentative(args) do
         when_type(Numeric) do
           raise "Shouldn't evaluate non-matching block"
@@ -33,11 +33,11 @@ describe Argumentative do
       end
     end
 
-    assert_equal 'Handled String', complicated_method("some string")
+    assert_equal 'Handled String', flexible_args_method("some string")
   end
 
   it 'runs clause for type when last matches' do
-    def complicated_method(*args)
+    def flexible_args_method(*args)
       argumentative(args) do
         when_type(String) do
           'Handled String'
@@ -49,11 +49,11 @@ describe Argumentative do
       end
     end
 
-    assert_equal 'Handled String', complicated_method("some string")
+    assert_equal 'Handled String', flexible_args_method("some string")
   end
 
   it 'passes original args through to the executed block' do
-    def complicated_method(*args)
+    def flexible_args_method(*args)
       argumentative(args) do
         when_type(String) do |string_arg|
           assert_equal "some string", string_arg
@@ -66,6 +66,6 @@ describe Argumentative do
       end
     end
 
-    assert_equal 'Handled String', complicated_method("some string")
+    assert_equal 'Handled String', flexible_args_method("some string")
   end
 end
